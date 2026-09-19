b9803ba6a56f53798c096d139216043b3b16916b
demetriusmferris-hue <demetrius.m.ferris@gmail.com>
2026-09-19 02:07:49 -1000

    week 2 fix

--- week1/scripts/getdates.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const year = new Date().getFullYear();
-    document.getElementById('currentyear').textContent = year;
-
-    const lastModified = new Date(document.lastModified);
-    document.getElementById('lastModified').textContent =
-        `Last modified: ${lastModified.toLocaleString()}`;
-})
